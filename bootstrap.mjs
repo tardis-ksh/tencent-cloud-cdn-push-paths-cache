1e10ba40d78bfc676737ca1e914d6a3a4a47c2bc
tardis <14971857@qq.com>
2025-02-01 17:00:52 +0800

    fix: deps problem

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,7 +1,12 @@
 import child_process from 'child_process';
 
+const baseConfig = {
+	stdio: 'inherit', cwd: process.cwd()
+};
+
 // 环境默认不含有 pnpm
-child_process.execSync('corepack enable', { stdio: 'inherit', cwd: process.cwd() });
-child_process.execSync('pnpm install', { stdio: 'inherit', cwd: process.cwd() });
+child_process.execSync('corepack enable', baseConfig);
+child_process.execSync('pnpm init', baseConfig);
+child_process.execSync('pnpm i @tardis-ksh/tencent @actions/core -S --no-audit --no-progress', baseConfig);
 
 import('./index.mjs');
