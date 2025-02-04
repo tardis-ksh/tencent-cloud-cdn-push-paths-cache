f4406d8fd4fba377e99eaa7d80234c3c69552e37
tardis <14971857@qq.com>
2025-02-04 17:15:00 +0800

    feat: add deps file check

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,12 +1,19 @@
 import child_process from 'child_process';
+import fs from 'fs/promises'
+import path from 'path'
 
+const depsFile = 'package.json';
 const baseConfig = {
 	stdio: 'inherit', cwd: process.cwd()
 };
 
 // 环境默认不含有 pnpm
 child_process.execSync('corepack enable', baseConfig);
-child_process.execSync('pnpm init', baseConfig);
+
+if (!(await fs.readFile(path.resolve(depsFile)))) {
+	child_process.execSync('pnpm init', baseConfig);
+}
+
 child_process.execSync('pnpm i @tardis-ksh/tencent @actions/core -S --no-audit --no-progress', baseConfig);
 
 import('./index.mjs');
